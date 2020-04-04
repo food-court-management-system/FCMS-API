7117114d38fc7abc05e90cc18a0221e3abf32579
Le Duong <duonglam360@gmail.com>
2020-04-04 08:47:00 +0700

    Enable Https for server, config security block all http request to server.

--- src/main/java/xiaolin/config/SecurityConfig.java
+++ src/main/java/xiaolin/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.requiresChannel().anyRequest().requiresSecure();
         http.csrf().disable().authorizeRequests()
                 .anyRequest().permitAll()
 //                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
