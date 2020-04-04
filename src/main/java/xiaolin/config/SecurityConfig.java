c2f8bc6464712ce6fd13c90863c9c44b2b5ebeb6
Nữ Nguyễn <ngtuyetnu1997@gmail.com>
2020-04-04 23:08:27 +0700

    CORS

--- src/main/java/xiaolin/config/SecurityConfig.java
+++ src/main/java/xiaolin/config/SecurityConfig.java
@@ -32,7 +32,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+        http.csrf().disable().cors().and().authorizeRequests()
                 .anyRequest().permitAll()
 //                .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
 //                .antMatchers("/v2/api-docs", "/configuration/**", "/swagger*/**", "/webjars/**").permitAll()
